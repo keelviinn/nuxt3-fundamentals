250b231fead241e84ba92e6a0c74202e580cbebc
Kelvin Oliveira <kelvin.dasilvaoliveira@betssongroup.com>
2023-09-26 09:09:03 +0200

    course updates

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/movies">Movies</NuxtLink> |
+    </nav>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/movies/[id].vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Route id: {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
