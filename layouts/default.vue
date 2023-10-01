b2b5309aedee476cb0629657509fb81425c2b31d
Kelvin Oliveira <keelviinn@gmail.com>
2023-10-01 22:31:33 +0200

    composables

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+const isLoggedIn = useIsLoggedIn()
+</script>
 <template>
-  <nav>
-    <NuxtLink to="/">Home</NuxtLink> |
-    <NuxtLink to="/movies">Movies</NuxtLink> |
+  <nav class="flex gap-3">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/movies">Movies</NuxtLink>
+    <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+    <a href="#" v-else @click.prevent="isLoggedIn = false">Logout</a>
   </nav>
   <slot></slot>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -2,14 +2,22 @@
 definePageMeta({
   layout: 'plain'
 })
+
+const isLoggedIn = useIsLoggedIn()
+
+function login() {
+  isLoggedIn.value = true
+  useRouter().push('/')
+}
 </script>
 
 <template>
-  <form>
+  <form @submit.prevent="login">
     <h1>Login</h1>
     <label for="username">Username</label>
     <input type="text" id="username" />
     <label for="password">Password</label>
     <input type="password" id="password" />
+    <button type="submit">Login</button>
   </form>
 </template>
